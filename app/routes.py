1d156a2b15ca3adbacc1a6be68fb15bf61fd61de
simas <simas@the-happiness-index.com>
2020-04-08 20:36:39 +0100

    chapter 2 [1] added conditional to index.html in jinja2 template [2] added blog.html and blog_page() web page

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,9 @@ from app import app
 
 def index():
 	user = {'username': 'Miguel'}
-	return render_template('index.html', title='Hone', user=user)
+	return render_template('index.html', title='Home', user=user)
+
+@app.route('/blog')
+def blog_page():
+	msg = {'welcome': 'Welcome to my first microblog :)'}
+	return render_template('blog.html', title='Blog', msg=msg)
