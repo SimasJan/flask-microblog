0cb0a1e4d5fd569f9de56e4a67633d8b44d59c3c
simas <simas@the-happiness-index.com>
2020-04-08 21:36:16 +0100

    chapter 2: template inheritance [1] created the base.html template [2] modified index.html and blog.html templates to extend base.html and kept only the relevant information ;

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,24 @@ from app import app
 
 def index():
 	user = {'username': 'Miguel'}
-	return render_template('index.html', title='Home', user=user)
+
+	# creating fake posts for vistors to see
+	posts = [
+		{
+			'author' : {'username': 'John'},
+			'body' : 'Beautiful day in Portland!'
+		},
+		{
+			'author' : {'username': 'Susan'},
+			'body' : 'The Avengers movie was so cool!'
+		},
+		{
+			'author' : {'username': 'Lily'},
+			'body' : 'Coronavirus is still having a tremendious impact on the global economy.'
+		},
+
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/blog')
 def blog_page():
