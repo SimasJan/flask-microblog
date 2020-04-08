8d697dd4839e425a932facf5f44515ac949040dc
simas <simas@the-happiness-index.com>
2020-04-08 22:38:35 +0100

    chapter 3: Web Forms - Login Form [1] Created forms.py file with LoginForm class object [2] created login.html file that extends base.html - a page for login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -25,7 +26,8 @@ def index():
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/blog')
-def blog_page():
-	msg = {'welcome': 'Welcome to my first microblog :)'}
-	return render_template('blog.html', title='Blog', msg=msg)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
